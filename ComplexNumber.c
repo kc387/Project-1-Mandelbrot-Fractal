9d6dddf76bd9a961b19c5fa693117e769e67e173
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-11 01:42:07 -0400

    newComplexNumber

--- ComplexNumber.c
+++ ComplexNumber.c
@@ -21,7 +21,7 @@ ComplexNumber* newComplexNumber(double real_component, double imaginary_componen
 { 
     //YOUR CODE HERE
     ComplexNumber *new_c = (ComplexNumber*) malloc (sizeof(ComplexNumber));
-    If (new_c != NULL){
+    if (new_c != NULL){
         new_c -> real = real_component;
         new_c -> imaginary = imaginary_component;
         return new_c;
