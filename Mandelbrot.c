c4c411dc71654ff0bf29b825391dae855891de08
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-11 11:50:59 -0400

    MandelbrotIterations

--- Mandelbrot.c
+++ Mandelbrot.c
@@ -11,6 +11,16 @@ If the threshold is not exceeded after maxiters, the function returns 0.
 u_int64_t MandelbrotIterations(u_int64_t maxiters, ComplexNumber * c, double threshold)
 {
     //YOUR CODE HERE
+    u_int64_t iters = 0;
+    ComplexNumber* z = newComplexNumber(0, 0);
+    while (iters < maxiters) {
+        ComplexNumber* m = newComplexNumber(0, 0);
+        m = ComplexProduct(ComplexProduct(z, z), c);
+        if (ComplexAbs(m) > threshold) {
+            return iters;
+        }
+        iters++;
+    }
     return 0;
 } 
 
