44272894cb558575d13f0a63fab14d4e8a922b46
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-12 22:11:42 -0400

    filetocolormap

--- ColorMapInput.c
+++ ColorMapInput.c
@@ -26,7 +26,32 @@ void freeMap(int length, uint8_t** colorMap)
 uint8_t** FileToColorMap(char* colorfile, int* colorcount)
 {
     //YOUR CODE HERE
-    
+    colorcount = 0;
+    FILE* cfile = fopen('colorfile', 'r');
+    if(cfile == NULL) {
+        return NULL;
+    }
+    int numcount;
+    if (fscanf(cfile, "%d", &numcount) != 1) {
+        return NULL;
+    }
+    uint8_t** carray;
+    uint8_t* rbg[3];
+    uint8_t r, b, g;
+    while(colorcoung < numcount) {
+        if (fscanf(cfile, "%hhu", "%hhu", "%hhu", r, b, g) != 3) {
+            return NULL;
+            }
+        else {
+            *rbg[0] = r;
+            *rbg[1] = b;
+            *rbg[2] = g;
+            **carray = *rbg;
+            carray++;
+            colorcount++;
+        }
+    }
+    return carray;
 }
 
 
