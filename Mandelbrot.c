f768dfe65065851db736fd510599d1461690cf47
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-14 17:48:50 -0400

    final

--- Mandelbrot.c
+++ Mandelbrot.c
@@ -45,6 +45,7 @@ void Mandelbrot(double threshold, u_int64_t max_iterations, ComplexNumber* cente
                 ComplexNumber* c = ComplexSum(center, inside);
                 if(c != NULL) {
                     *output = MandelbrotIterations(max_iterations, c, threshold);
+                    printf("OUTPUT: %lu\n", *output);
                     output++;
                     free(c);
                     free(inside);
