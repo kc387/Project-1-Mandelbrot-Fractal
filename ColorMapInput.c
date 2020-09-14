cf38d75a289d7ac358b45dbffc73847ec55086a7
Kathleen Chen <kathleen.chen686@duke.edu>
2020-09-13 22:11:15 -0400

    filetocolormap

--- ColorMapInput.c
+++ ColorMapInput.c
@@ -17,6 +17,9 @@
 void freeMap(int length, uint8_t** colorMap)
 {
     //YOUR CODE HERE 
+    for(int i = 0; i < length; i ++) {
+        free(*(colorMap + i));
+    }
     free(colorMap);
 }
 
@@ -27,43 +30,34 @@ void freeMap(int length, uint8_t** colorMap)
 uint8_t** FileToColorMap(char* colorfile, int* colorcount)
 {
     //YOUR CODE HERE
-    colorcount = 0;
     FILE* cfile = fopen(colorfile, "r");
     if(cfile == NULL) {
         return NULL;
     }
-    int numcount;
-    if (fscanf(cfile, "%d", &numcount) != 1) {
+    if (fscanf(cfile, "%d", colorcount) != 1) {
         return NULL;
-    }
-    else{
-        fscanf(cfile, "%d", &numcount);
     }
 
     uint8_t** carray = (uint8_t**) malloc (sizeof(uint8_t*));
-    uint8_t* rgb[3];
     int r, g, b;
-    while(*colorcount < numcount) {
+    for(int i = 0; i < *colorcount; i++) {
         if (fscanf(cfile, "%d %d %d", &r, &g, &b) != 3) {
             return NULL;
             }
         else {
-            if (r > 255 || r < 0) {
-                r = 0;
+            if (r > 255 || r < 0 || g > 255 || g < 0 || b > 255 || b < 0) {
+                freeMap(i, carray);
+                return NULL;
             }
-            if (g > 255 || g < 0) {
-                g = 0;
-            }
-            if (b > 255 || b < 0) {
-                b = 0;
-            }
-            //uint8_t* carray = (uint8_t*) malloc (3 * sizeof(uint8_t));
-            *rgb[0] = (uint8_t)r;
-            *rgb[1] = (uint8_t)g;
-            *rgb[2] = (uint8_t)b;
-            //**carray = *rgb;
+            uint8_t* rgb = (uint8_t*) malloc (3 * sizeof(uint8_t));
+            *rgb = (uint8_t)r;
+            rgb ++;
+            *rgb = (uint8_t)g;
+            rgb ++;
+            *rgb = (uint8_t)b;
+            **carray = *rgb;
             carray++;
-            colorcount++;
+            free(rgb);
         }
     }
     fclose(cfile);
